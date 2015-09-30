67314d98a9e5ff43831e0b00f79125c2c2e2595c
Rhisereld <Rhisereld@gmail.com>
2015-09-30 20:36:57 +1000

    Added explanatory comments to saveSchematic() and loadSchematic(). These functions now return void.

--- src/main/java/com/gmail/Rhisereld/HorizonShips/SchematicManager.java
+++ src/main/java/com/gmail/Rhisereld/HorizonShips/SchematicManager.java
@@ -26,6 +26,14 @@ public class SchematicManager
 	private CuboidClipboard clipboard;
 	private File file;
 	
+	/**
+	 * Constructor: Creates a SchematicManager object that contains an EditSession detailing all the changes that have been made.
+	 * 				The object can be retained for purposes of undoing/redoing.
+	 * 				Can only apply to one world (create another for a different world).
+	 * 
+	 * @param world - the world to which this session applies.
+	 * @return SchematicManager
+	 */
 	public SchematicManager(World world)
 	{
     	wep = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
@@ -33,7 +41,17 @@ public class SchematicManager
 		editSession = worldEdit.getEditSessionFactory().getEditSession(new BukkitWorld(world), 10000000);
 	}
 	
-	public boolean saveSchematic(Location loc1, Location loc2, String schematicName) throws DataException, IOException
+	/**
+	 * saveSchematic() copies the blocks between the two locations given (forming the two opposing apexes of a cuboid)
+	 * and saves them to a schematic in \plugins\HorizonShips\schematics\.
+	 * 
+	 * @param loc1 - the first apex of the cuboid
+	 * @param loc2 - the second apex of the cuboid
+	 * @param schematicName - the name under which the schematic will be saved.
+	 * @throws DataException
+	 * @throws IOException
+	 */
+	public void saveSchematic(Location loc1, Location loc2, String schematicName) throws DataException, IOException
 	{
 		file = new File("plugins\\HorizonShips\\schematics\\" + schematicName + ".schematic");
 		file.getParentFile().mkdirs(); //Ensure the directory exists.
@@ -46,10 +64,19 @@ public class SchematicManager
 		clipboard.copy(editSession);
 		MCEditSchematicFormat.MCEDIT.save(clipboard, file);
 		editSession.flushQueue();		
-		return true;
 	}
 
-	public boolean loadSchematic(String schematicName, Location l) throws MaxChangedBlocksException, DataException, IOException
+	/**
+	 * loadSchematic() loads the schematic found in \plugins\HorizonShips\schematics\ and pastes them at the
+	 * origin provided.
+	 * 
+	 * @param schematicName - the name of the schematic to be loaded.
+	 * @param l - the location at which to paste the schematic into the world.
+	 * @throws MaxChangedBlocksException
+	 * @throws DataException
+	 * @throws IOException
+	 */
+	public void loadSchematic(String schematicName, Location l) throws MaxChangedBlocksException, DataException, IOException
 	{
 		file = new File("plugins\\HorizonShips\\schematics\\" + schematicName + ".schematic");
 		Vector origin = new Vector(l.getX(), l.getY(), l.getZ());
@@ -59,9 +86,14 @@ public class SchematicManager
 		//Load schematic into clipboard.
 		MCEditSchematicFormat.MCEDIT.load(file).paste(editSession, origin, true, true);
 		editSession.flushQueue();
-		return true;
 	}
 	
+	/**
+	 * getMin() returns a Vector containing the minimum point of the cuboid specified by two locations.
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
 	private Vector getMin(Location l1, Location l2) 
 	{
 		return new Vector(Math.min(l1.getBlockX(), l2.getBlockX()),
@@ -69,6 +101,12 @@ public class SchematicManager
 		                  Math.min(l1.getBlockZ(), l2.getBlockZ()));
 	}
 
+	/**
+	 * getMax() returns a Vector containing the maximum point of the cuboid specified by two locations.
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
 	private Vector getMax(Location l1, Location l2) 
 	{
 		return new Vector(Math.max(l1.getBlockX(), l2.getBlockX()),
