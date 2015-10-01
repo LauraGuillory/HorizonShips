0bd55dffa87c9325cff0f4318145c481dfc42245
Rhisereld <Rhisereld@gmail.com>
2015-10-01 13:06:14 +1000

    Added confirmation for the command /ship create.

--- src/main/java/com/gmail/Rhisereld/HorizonShips/ShipsCommandExecutor.java
+++ src/main/java/com/gmail/Rhisereld/HorizonShips/ShipsCommandExecutor.java
@@ -1,6 +1,7 @@
 package com.gmail.Rhisereld.HorizonShips;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -8,15 +9,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.fusesource.jansi.Ansi.Color;
-
-import com.sk89q.worldedit.bukkit.selections.Selection;
 import com.sk89q.worldedit.data.DataException;
 
+@SuppressWarnings("deprecation")
 public class ShipsCommandExecutor implements CommandExecutor 
 {
 	ConfigAccessor data;
 	Ship ship;
+	
+	HashMap<String, String> confirmCreate = new HashMap<String, String>();	//Used to confirm commands
 	
     public ShipsCommandExecutor(ConfigAccessor data) 
     {
@@ -30,12 +31,11 @@ public class ShipsCommandExecutor implements CommandExecutor
      * 
      * @see org.bukkit.command.CommandExecutor#onCommand(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
      */
-	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) 
 	{
-		Player player;
 		String name = sender.getName();
+		Player player = Bukkit.getPlayer(name);
 		String[] arguments;
 		
 		//All commands that fall under /ship [additional arguments]
@@ -49,9 +49,6 @@ public class ShipsCommandExecutor implements CommandExecutor
 			//ship create [shipName] [destinationName]
 			if (args[0].equalsIgnoreCase("create"))
 			{
-				player = Bukkit.getPlayer(name);
-				SchematicManager sm = new SchematicManager(player.getWorld());
-				
 				//Check for correct number of arguments.
 				if (args.length < 3)
 				{
@@ -63,25 +60,71 @@ public class ShipsCommandExecutor implements CommandExecutor
 					sender.sendMessage(ChatColor.RED + "Too many arguments! Correct usage: /ship create [shipName] [destinationName]");
 					return false;
 				}
-				
-				try {
-					ship.createShip(args[1], player, args[2]);
-				} catch (DataException | IOException e) {
-					player.sendMessage(Color.RED + "Couldn't create ship. Please report this to an Adminstrator.");
-					e.printStackTrace();
-					return false;
-				} catch (NullPointerException e) {
-					sender.sendMessage(ChatColor.RED + "No ship selected. Please make a selection using WorldEdit.");
-					return false;
-				} catch (IllegalArgumentException e) {
-					sender.sendMessage(ChatColor.RED + "A ship already exists by that name.");
-					return false;
-				}
+
+				sender.sendMessage(ChatColor.YELLOW + "A ship will be created using your current WorldEdit selection. Is this correct?"
+						+ " Type '/ship confirm create' to confirm.");
+				confirmCreate.put(name, args[1] + " " + args[2]);
+				confirmCreateTimeout(sender, name);
 				
 				return true;
 			}
+			
+			//ship confirm
+			if (args[0].equalsIgnoreCase("confirm"))
+			{
+				if (args.length != 2)
+				{
+					sender.sendMessage(ChatColor.RED + "Incorrect format.");
+					return true;
+				}
+				
+				//ship confirm create
+				if (args[1].equalsIgnoreCase("create"))
+				{					
+					if (confirmCreate.get(name) == null)
+					{
+						sender.sendMessage(ChatColor.RED + "There is nothing for you to confirm.");
+						return true;
+					}
+					
+					arguments = confirmCreate.get(name).split(" ");
+					confirmCreate.remove(name);
+
+					try {
+						ship.createShip(arguments[0], player, arguments[1]);
+					} catch (DataException | IOException e) {
+						player.sendMessage(ChatColor.RED + "Couldn't create ship. Please report this to an Adminstrator.");
+						e.printStackTrace();
+						return false;
+					} catch (NullPointerException e) {
+						sender.sendMessage(ChatColor.RED + "No ship selected. Please make a selection using WorldEdit.");
+						return false;
+					} catch (IllegalArgumentException e) {
+						sender.sendMessage(ChatColor.RED + "A ship already exists by that name.");
+						return false;
+					}
+
+					player.sendMessage(ChatColor.YELLOW + "Ship " + arguments[0] + " created!");
+					return true;
+				}
+			}
 		}
 		return false;
+	}
+	
+	private void confirmCreateTimeout(final CommandSender sender, final String key)
+	{
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+		{
+			public void run() 
+			{
+				if (confirmCreate.containsKey(key))
+				{
+					confirmCreate.remove(key);
+					sender.sendMessage(ChatColor.RED + "You timed out.");
+				}
+			}			
+		} , 200);
 	}
 
 }
