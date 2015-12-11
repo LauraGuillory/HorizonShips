eba230de7c6d8b639ccbd900b6e88d8db6dcb7d4
Rhisereld <Rhisereld@gmail.com>
2015-12-12 07:34:50 +1000

    Anyone who is not the ship owner or a permitted pilot is now blocked from placing blocks within the region.

--- src/main/java/com/gmail/Rhisereld/HorizonShips/ShipProtector.java
+++ src/main/java/com/gmail/Rhisereld/HorizonShips/ShipProtector.java
@@ -9,7 +9,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
+/**
+ * ShipProtector listens for block breaking or block placing and prevents these events from occuring within a ship
+ * unless the actor is the ship's owner or a permitted pilot.
+ * 
+ * @author Rhisereld
+ */
 public class ShipProtector implements Listener
 {
 	ConfigAccessor data;
@@ -21,6 +28,11 @@ public class ShipProtector implements Listener
 		this.config = config;
 	}
 	
+	/**
+	 * onBreakBlock() is triggered each time a block is broken.
+	 * 
+	 * @param event
+	 */
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onBreakBlock(BlockBreakEvent event)
 	{
@@ -56,12 +68,55 @@ public class ShipProtector implements Listener
 					{
 						player.sendMessage(ChatColor.RED + "You don't own this ship!");
 						event.setCancelled(true);
-					}
-						
+					}	
 				}
 			}
+		}
+	}
+	
+	/**
+	 * onBlockPlace() is triggered every time a block is placed.
+	 * 
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onBlockPlace(BlockPlaceEvent event)
+	{
+		Set<String> ships = data.getConfig().getConfigurationSection("ships").getKeys(false);
+		
+		Ship ship;
+		Location min;
+		Location max;
+		Player player = event.getPlayer();
+		int x = event.getBlock().getX();
+		int y = event.getBlock().getY();
+		int z = event.getBlock().getZ();
+		
+		//Check if the block is in any destination of any ship.
+		for (String s: ships)
+		{
+			ship = new Ship(data, s);
+			Set<String> destinations = ship.getAllDestinations();
 			
-
+			for (String d: destinations)
+			{
+				min = ship.getDestination(d).getLocation();
+				max = new Location(min.getWorld(), min.getBlockX() + ship.getLength(), min.getBlockY() + ship.getHeight(), 
+						min.getBlockY() + ship.getWidth());
+				
+				if (min.getWorld().equals(player.getLocation().getWorld()) 
+						&& min.getBlockX() < x && max.getBlockX() > x
+						&& min.getBlockY() < y && max.getBlockY() > y
+						&& min.getBlockZ() < z && max.getBlockZ() > z)
+				{
+					//If the player isn't the owner or a permitted pilot, notify them and cancel the event.
+					if ((ship.getOwner() == null || ship.getOwner().equals(player.getUniqueId())) || ship.isPilot(player.getUniqueId()))
+					{
+						player.sendMessage(ChatColor.RED + "You don't own this ship!");
+						event.setCancelled(true);
+					}		
+				}
+			}
 		}
 	}
 }
